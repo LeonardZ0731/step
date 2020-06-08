eab16a85a82795a0f0d6de1348984bc72ce1e7ba
Leonard Zhang <zyw0731@hotmail.com>
2020-06-08 20:16:08 +0000

    Add a map using the Google Map API to the website and adjust its style
    Modify the index.html file to add a division for the map. Update the script.js file to include a function that initializes the map.
    style.css file is also updated to prevent the style applied to the div tag from modifying the background and contents of the map.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -55,6 +55,7 @@ function displayComments(maxComments, comments) {
         var button = document.createElement("button");
         button.innerHTML = "Like";
         button.type = "button";
+        button.className = "like-button";
         const input = keyString.slice();
         button.addEventListener("click", function () { likeComments(input) });
         commentContainer.appendChild(para);
@@ -103,4 +104,155 @@ async function likeComments(keyString) {
     const queryURL = "/comments/like?key=" + keyString;
     const request = new Request(queryURL, {method: "POST"});
     const newResponse = await fetch(request);
+}
+
+/**
+ * Initialize a map on the page
+ */
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 40.44230, lng: -79.9427},
+        zoom: 18,
+        styles: [
+            {
+                "elementType": "geometry",
+                "stylers": [{"color": "#ebe3cd"}]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#523735"}]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{"color": "#f5f1e6"}]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#c9b2a6"}]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#dcd2be"}]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#ae9e90"}]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#36aff9"}]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dfd2ae"}]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dfd2ae"}]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#93817c"}]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [{"color": "#a5b076"}]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#28c4fa"}, {"lightness": -5}, {"weight": 2}]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.stroke",
+                "stylers": [{"color": "#f9fcc7"}]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [{"color": "#f5f1e6"}]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [{"color": "#fdfcf8"}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{"color": "#f8c967"}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#f2756a"}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#f98357"}]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [{"color": "#e98d58"}]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#db8555"}]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#806b63"}]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dfd2ae"}]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#8f7d77"}]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.stroke",
+                "stylers": [{"color": "#ebe3cd"}]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dfd2ae"}]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [{"color": "#65d3f9"}, {"saturation": -10},  {"lightness": 10}]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#92998d"}]
+            }
+        ],
+        mapTypeId: "satellite"
+    });
+    map.setTilt(45);
+}
+
+async function initialize() {
+    initMap();
+    fetchCommentsWithStoredLimit();
 }
