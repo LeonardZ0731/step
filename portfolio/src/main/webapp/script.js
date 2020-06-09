98dda206268981783c2d8f9386ba9327c5f810b5
Leonard Zhang <zyw0731@hotmail.com>
2020-06-09 21:12:58 +0000

    Add a search box to the map
    Update the index.html file to include a search box at the top of the map.
    Modify the initMap() function to generate a search box when the map is initiated. When search for a places, the result will be restricted such that the top
    results will near the current viewport.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -272,6 +272,36 @@ async function initMap() {
         var lng = parseFloat(latLng.longitude);
         displayMarker(lat, lng);
     }
+
+    var input = document.getElementById("place-input");
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Restrict the search result near to the current viewport
+    map.addListener("bounds_changed", function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    // Place a new marker on the place that the user searches
+    searchBox.addListener("places_changed", function() {
+        var places = searchBox.getPlaces();
+        if (places.length === 0) {
+            return;
+        }
+
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                return;
+            }
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
 
 function displayMarker(lat, lng) {
