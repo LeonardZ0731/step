a384b6cdf3647a75637ec3d575cc9f75ccc37b4a
Leonard Zhang <zyw0731@hotmail.com>
2020-06-11 23:48:51 +0000

    Forgot to include the modified script.js file in the last commit. I included the file in this commit.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -340,7 +340,7 @@ async function addMarker(position) {
 async function checkStatus() {
     const response = await fetch('/login/status');
     const statusResponse = await response.json();
-    const status = statusRespone[0];
+    const status = statusResponse[0];
     if (status) {
         document.getElementById("comments-login").style.display = "none";
         document.getElementById("comments-logout").style.display = "block";
