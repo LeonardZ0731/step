a59907092b02c274a6c0a5f065982dba1a189697
Leonard Zhang <zyw0731@hotmail.com>
2020-06-15 22:40:45 +0000

    Implement the query() in the FindMeetingQuery.java file and pass the tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,53 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+      long duration = request.getDuration();
+      if (duration > TimeRange.WHOLE_DAY.duration()) {
+          return Arrays.asList();
+      }
+      Collection<String> attendees = request.getAttendees();
+      Set<String> attendeeSet = new HashSet<>(attendees);
+
+      List<TimeRange> validEvents = new ArrayList<>();
+      for (Event event: events) {
+          Set<String> copy = new HashSet<>(attendees);
+          copy.retainAll(event.getAttendees());
+          if (copy.size() > 0) {
+              validEvents.add(event.getWhen());
+          }
+      }
+      Collections.sort(validEvents, TimeRange.ORDER_BY_START);
+      TimeRange initial = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
+      List<TimeRange> result = new ArrayList<>();
+      for (TimeRange range: validEvents) {
+          if (range.contains(initial)) {
+              break;
+          }
+          if (range.end() <= initial.start()) {
+              continue;
+          }
+          if (range.start() <= initial.start()) {
+              initial = TimeRange.fromStartEnd(range.end(), TimeRange.END_OF_DAY, true);
+              continue;
+          }
+          TimeRange temp = TimeRange.fromStartEnd(initial.start(), range.start(), false);
+          if (temp.duration() >= duration) {
+              result.add(TimeRange.fromStartEnd(initial.start(), range.start(), false));
+          }
+          initial = TimeRange.fromStartEnd(range.end(), TimeRange.END_OF_DAY, true);
+      }
+      if (initial.start() != initial.end() && initial.duration() >= duration) {
+          result.add(initial);
+      }
+      return result;
   }
 }
