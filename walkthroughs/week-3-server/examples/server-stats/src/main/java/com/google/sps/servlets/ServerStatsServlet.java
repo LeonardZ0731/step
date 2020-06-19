1cbecbe19af44396048ab66223ac3047a2f223ed
Leonard Zhang <zyw0731@hotmail.com>
2020-06-19 16:07:45 +0000

    Commit the changes made when I completed the walkthrough exercises

--- walkthroughs/week-3-server/examples/server-stats/src/main/java/com/google/sps/servlets/ServerStatsServlet.java
+++ walkthroughs/week-3-server/examples/server-stats/src/main/java/com/google/sps/servlets/ServerStatsServlet.java
@@ -37,6 +37,7 @@ public final class ServerStatsServlet extends HttpServlet {
 
     // Convert the server stats to JSON
     ServerStats serverStats = new ServerStats(startTime, currentTime, maxMemory, usedMemory);
+    ServerStats serverStats2 = new ServerStats(startTime, curretn)
     String json = convertToJson(serverStats);
 
     // Send the JSON as the response
